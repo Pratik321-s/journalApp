a813a0721c29f859e7943e7dde6663d17c565a87
Pratik321-s <153607554+Pratik321-s@users.noreply.github.com>
2025-08-21 22:53:58 +0530

    first commit

---
+++ src/main/java/com/pratik/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.pratik.journalApp.repository;
+
+import com.pratik.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
---
+++ src/main/java/com/pratik/journalApp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pratik.journalApp.repository;
+
+import com.pratik.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    User findByUserName(String username);
+
+    void deleteByUserName(String username);
+}
